ce217021c15a4be420dd5d953ccdf67abb55f000
Shawn Gamble <sagamble10@gmail.com>
2025-04-28 22:57:32 -0400

    Birds

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,19 @@
-const History = document.querySelector("#history");
+// script.js
+document.getElementById('joinForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const messageDiv = document.getElementById('message');
+    if (name) {
+        messageDiv.innerHTML = `Thank you for joining, ${name}!`;
+    } else {
+        messageDiv.innerHTML = 'Please enter your name.';
+    }
+});
 
-History.onclick = function () {
-    alert("History button clicked!");
-};
+document.getElementById('subscribe').addEventListener('change', function() {
+    alert('Thank you for subscribing!');
+});
+
+document.getElementById('playerInfo').addEventListener('click', function() {
+    this.innerHTML = '<p>More information about the players will be available soon!</p>';
+});
